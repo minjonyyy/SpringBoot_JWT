769f7e834a09b764093abc26ffafb8ec36c930cf
minjonyyy <minjonyyy@naver.com>
2025-06-12 13:30:59 +0900

    refactor : 데이터베이스 연동하여 수정

--- src/main/java/com/example/springboot_jwt/auth/entity/User.java
+++ src/main/java/com/example/springboot_jwt/auth/entity/User.java
@@ -1,17 +1,44 @@
 package com.example.springboot_jwt.auth.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
-    private UserRole role;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id")
+    )
+    @Enumerated(EnumType.STRING)
+    private Set<UserRole> roles = new HashSet<>(Set.of(UserRole.ROLE_USER));
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
 
 }
